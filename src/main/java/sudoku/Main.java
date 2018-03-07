351990dd47b564ae2542c49a19022bb49f4b6d4f
Florke07 <maciek.socha1@gmail.com>
2018-03-07 23:43:27 +0100

    Test IntelliJ commit

--- src/main/java/sudoku/Main.java
+++ src/main/java/sudoku/Main.java
@@ -3,6 +3,7 @@ package sudoku;
 public class Main {
     public static void main(final String[] args) {
         System.out.println("Hello World");
+        System.out.println("Chyba dziala");
 
     }
 }
