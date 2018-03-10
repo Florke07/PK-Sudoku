4c814f8a5a8b4662dd6d1edfa3f53ee479404562
Florke07 <maciek.socha1@gmail.com>
2018-03-10 17:53:52 +0100

    metoda isOK powinna sprawdzać czy dobra liczba

--- src/main/java/sudoku/SudokuBoard.java
+++ src/main/java/sudoku/SudokuBoard.java
@@ -28,4 +28,27 @@ public class SudokuBoard {
             System.out.println();
         }
     }
+
+    public boolean isOK(int currentRowPosition, int currentColumnPosition, int valueToCheck){
+
+        for(int i=0;i<9;i++)
+            if (board[currentRowPosition][i] == valueToCheck) return false;
+
+        for (int i=0;i<9;i++)
+            if (board[i][currentColumnPosition] == valueToCheck) return false;
+
+
+        int squareFirstRowNumber = 3 * (currentRowPosition/3);
+        int squareFirstColumnNumber = 3 * (currentColumnPosition/3);
+
+        int squareEndRowNumber = squareFirstRowNumber + 2;
+        int squareEndColumnNumber = squareFirstColumnNumber + 2;
+
+        for (int x = squareFirstRowNumber; x <= squareEndRowNumber; x++)
+            for (int y = squareFirstColumnNumber; y <= squareEndColumnNumber; y++)
+                if (board[x][y] == valueToCheck) return false;
+
+
+        return true;
+    }
 }
