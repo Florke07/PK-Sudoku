5839498d8fdabf15cc728c2b7bc19f3538c654b7
Florke07 <maciek.socha1@gmail.com>
2018-03-10 16:49:24 +0100

    0000

--- src/main/java/sudoku/Main.java
+++ src/main/java/sudoku/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(final String[] args) {
         System.out.println("Hello World");
         System.out.println("Chyba dziala");
-
+        SudokuBoard sud = new SudokuBoard();
+        sud.show();
     }
 }
--- src/main/java/sudoku/SudokuBoard.java
+++ src/main/java/sudoku/SudokuBoard.java
@@ -1,13 +1,31 @@
 package sudoku;
 
 public class SudokuBoard {
-    private int[][] board = new int[9][9];
+
+    private int[][] board = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
 
     public SudokuBoard() {
-        //todo wypełnienie poczatkowe tablicy
     }
 
     public void fillBoard(){
         //todo automatyczne rozwiazanie sudoku algorytmem backtrackingu
+    }
+
+    public void show(){
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
