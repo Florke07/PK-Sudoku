eb46c9e6eedc737e2460442925e657ad70da84f5
Florke07 <maciek.socha1@gmail.com>
2018-03-10 16:15:56 +0100

    up

--- src/main/java/sudoku/SudokuBoard.java
+++ src/main/java/sudoku/SudokuBoard.java
@@ -1,4 +1,13 @@
 package sudoku;
 
 public class SudokuBoard {
+    private int[][] board = new int[9][9];
+
+    public SudokuBoard() {
+        //todo wypełnienie poczatkowe tablicy
+    }
+
+    public void fillBoard(){
+        //todo automatyczne rozwiazanie sudoku algorytmem backtrackingu
+    }
 }
