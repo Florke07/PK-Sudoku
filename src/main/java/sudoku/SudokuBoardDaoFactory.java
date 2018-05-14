a2e03e4923b2027fa4cbe0f3cf88ece41c932de7
Florke07 <maciek.socha1@gmail.com>
2018-05-14 19:31:45 +0200

    test commit

--- src/main/java/sudoku/SudokuBoardDaoFactory.java
+++
@@ -1,8 +0,0 @@
-package sudoku;
-
-public class SudokuBoardDaoFactory {
-    public Dao getFileDao(String fileName) {
-        FileSudokuBoardDao dao = new FileSudokuBoardDao(fileName);
-        return dao;
-    }
-}
