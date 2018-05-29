904d761d80b500ba099dadaacd6d8b749dbc5919
Florke07 <maciek.socha1@gmail.com>
2018-05-29 12:01:19 +0200

    k

--- View/src/sample/View.java
+++ View/src/sample/View.java
@@ -18,6 +18,8 @@ import org.slf4j.LoggerFactory;
 import sudoku.Dao;
 import sudoku.SudokuBoard;
 import sudoku.SudokuBoardDaoFactory;
+import sudokupart.SudokuBox;
+import sudokupart.SudokuField;
 
 import java.util.ArrayList;
 
@@ -36,7 +38,7 @@ public class View extends Application {
     final Logger logger = LoggerFactory.getLogger(View.class);
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         int width = 300;
         int height = 275;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -57,21 +59,21 @@ public class View extends Application {
         primaryStage.setScene(new Scene(basic, width, height));
         primaryStage.show();
 
-        Izi.setPrefSize(100,20);
+        Izi.setPrefSize(100, 20);
         Izi.setOnAction(e -> {
             FieldsRemover.removeField(Difficulty.EASY, sb);
             logger.info("Easy");
             setScene();
             primaryStage.setScene(playBord);
         });
-        Medium.setPrefSize(100,20);
+        Medium.setPrefSize(100, 20);
         Medium.setOnAction(e -> {
             FieldsRemover.removeField(Difficulty.MEDIUM, sb);
             logger.info("Medium");
             setScene();
             primaryStage.setScene(playBord);
         });
-        Hard.setPrefSize(100,20);
+        Hard.setPrefSize(100, 20);
         Hard.setOnAction(e -> {
             FieldsRemover.removeField(Difficulty.HARD, sb);
             logger.info("Hard");
@@ -94,8 +96,7 @@ public class View extends Application {
             getFields();
             if (sb.checkBoard()) {
                 logger.info("Good");
-            }
-            else {
+            } else {
                 logger.info("Bad");
             }
         });
@@ -111,40 +112,49 @@ public class View extends Application {
         BorderPane trueGameLayout = new BorderPane();
         GridPane gameLayout = new GridPane();
         gameLayout.setGridLinesVisible(true);
-        gameLayout.setPadding(new Insets(5,5,5,5));
+        gameLayout.setPadding(new Insets(5, 5, 5, 5));
         gameLayout.setHgap(8);
         gameLayout.setVgap(8);
-        gameLayout.addRow(4);
+        gameLayout.addRow(3);
         gameLayout.addColumn(3);
         int k = 0;
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 GridPane innergrid = new GridPane();
                 innergrid.setGridLinesVisible(false);
-                innergrid.setPadding(new Insets(8,8,8,8));
+                innergrid.setPadding(new Insets(8, 8, 8, 8));
                 innergrid.setHgap(8);
                 innergrid.setVgap(8);
                 innergrid.addRow(3);
                 innergrid.addColumn(3);
-                for(int x = 1;x <= 3; x++){
-                    for(int y = 1;y <= 3;y++){
-                        TextField tx = new TextField(String.valueOf(sb.getValue((i*x)-1, (j*y)-1)));
-                        tx.setPrefWidth(35);
-                        tx.setPrefHeight(35);
-                        if (!sb.getField((i*x)-1,(j*y)-1).isModifiable()) tx.setDisable(true);
-                        fields.add(tx);
-                        innergrid.add(fields.get(k),x-1,y-1);
-                        k++;
-                    }
+
+                SudokuBox sudokuBox;
+                sudokuBox = sb.getBox(i, j);
+                for(SudokuField h : sudokuBox.elements) {
+                    System.out.print(h.getFieldValue());
+                    System.out.print(h.isModifiable());
                 }
-                gameLayout.add(innergrid, j-1, i-1);
+                for (int x = 0; x < 9; x++) {
+                    //System.out.println("dupa");
+                    System.out.println(sudokuBox.elements.get(x).getFieldValue());
+                    System.out.println(sudokuBox.elements.get(x).isModifiable());
+                    TextField tx = new TextField(String.valueOf(sudokuBox.elements.get(x).getFieldValue()));
+                    tx.setPrefWidth(35);
+                    tx.setPrefHeight(35);
+                    if (!sudokuBox.elements.get(x).isModifiable()) tx.setDisable(true);
+                    fields.add(tx);
+                    innergrid.add(fields.get(k), x%3, x/3);
+                    k++;
+                }
+                gameLayout.add(innergrid, i, j);
 
             }
         }
+        //System.out.println(sb.toString());
         trueGameLayout.setCenter(gameLayout);
         HBox options = new HBox();
         options.setSpacing(50);
-        options.setPadding(new Insets(5,5,5,75));
+        options.setPadding(new Insets(5, 5, 5, 75));
         options.getChildren().add(save);
         options.getChildren().add(load);
         options.getChildren().add(check);
@@ -154,11 +164,17 @@ public class View extends Application {
     }
 
     private void getFields() {
-        int k=0;
-        for (int i=0;i<9;i++) {
-            for (int j=0;j<9;j++) {
-                sb.setValue(i,j,Integer.parseInt(fields.get(k).getText()));
-                k++;
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for(int x = 0; x<3;x++){
+                    for(int y = 0; y<3;y++){
+                        sb.setValue((i*3)+x,(j*3)+y,Integer.parseInt(fields.get(k).getText()));
+                        k++;
+                    }
+                }
+                //sb.setValue(i, j, Integer.parseInt(fields.get(k).getText()));
+                //k++;
             }
         }
     }
